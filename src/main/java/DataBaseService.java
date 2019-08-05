c926687c97b6d8361116ceddaae04c96ed5e87a5
Vladimir Iakimov <yakimovvn@bk.ru>
2019-08-06 00:27:43 +0300

    Улучшил методы ввода. Сделал базу данных для blacklist

--- src/main/java/DataBaseService.java
+++ src/main/java/DataBaseService.java
@@ -18,64 +18,48 @@ class DataBaseService {
 
     static synchronized String getNickByLoginAndPass(String login, String pass) {
         String sql = String.format("SELECT nickname FROM main where login = '%s' and password = '%s'", login, pass);
-
         try {
             ResultSet rs = stmt.executeQuery(sql);
-
             if(rs.next()) {
                 return rs.getString(1);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
     static synchronized void addToBlackList(String holderBlackList, String userToBlackList){
-        String sql = String.format("SELECT blacklist FROM main where nickname = '%s'", holderBlackList);
         try {
-            ResultSet rs = stmt.executeQuery(sql);
-            if(rs.next()) {
-                String newBlackList = (rs.getString(1) == null)? userToBlackList
-                        : rs.getString(1)+" "+userToBlackList;
-                sql = String.format("UPDATE main SET blacklist = '%s' WHERE nickname = '%s'",newBlackList,holderBlackList);
-                stmt.execute(sql);
-            }
-
+            String sql = String.format("INSERT INTO blacklist (id_nick,id_black) VALUES (%s,%s)",getId(holderBlackList),getId(userToBlackList));
+            stmt.execute(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    static synchronized boolean isInBlackList (String holderBlackList, String userForCheck){
+    static synchronized boolean isInBlackList (String holderBlackList, String userForCheck) {
         boolean res = false;
-        String sql = String.format("SELECT blacklist FROM main where nickname = '%s'", holderBlackList);
+
         try {
+            String sql = String.format("SELECT  main.nickname as 'black' FROM main\n" +
+                    "INNER JOIN blacklist ON main.id = blacklist.id_black\n" +
+                    "WHERE blacklist.id_nick = %s and main.nickname ='%s'",getId(holderBlackList),userForCheck);
             ResultSet rs = stmt.executeQuery(sql);
-            if(rs.next() && rs.getString(1)!= null){
-                String[] blackListArr = rs.getString(1).split(" ");
-                for (String user: blackListArr) {
-                    if(user.equals(userForCheck)) {
-                        res = true;
-                        break;
-                    }
-                }
-            }
+            res = rs.next();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return res;
+
     }
+
     private static synchronized boolean isUserWithLogin(String login){
         boolean isUser = false;
         String sql = String.format("SELECT id FROM main where login = '%s'", login );
         try {
             ResultSet rs = stmt.executeQuery(sql);
-            if(rs.next()){
-                isUser = true;
-            }
+            isUser = rs.next();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -87,9 +71,7 @@ class DataBaseService {
         String sql = String.format("SELECT id FROM main where nickname = '%s'", nick );
         try {
             ResultSet rs = stmt.executeQuery(sql);
-            if(rs.next()){
-                isUser = true;
-            }
+            isUser = rs.next();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -97,50 +79,56 @@ class DataBaseService {
 
     }
 
+
     static synchronized void deleteFromBlackList (String holderBlackList, String userDeleteFromBlackList){
-        String sql = String.format("SELECT blacklist FROM main where nickname = '%s'", holderBlackList);
         try {
-            ResultSet rs = stmt.executeQuery(sql);
-            if(rs.next() && rs.getString(1)!= null){
-                StringBuilder sb = new StringBuilder();
-                String[] blackListArr = rs.getString(1).split(" ");
-                for (String user: blackListArr) {
-                    if(!user.equals(userDeleteFromBlackList)){
-                        sb.append(user);
-                        sb.append(" ");
-                    }
-                }
-                sql = String.format("UPDATE main SET blacklist = '%s' WHERE nickname = '%s'",sb.toString(),holderBlackList);
-                stmt.execute(sql);
-
-            }
+            String sql = String.format("DELETE FROM blacklist WHERE id_nick = %s and id_black = %s"
+                    ,getId(holderBlackList),getId(userDeleteFromBlackList));
+            stmt.execute(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
     }
 
-    static synchronized String registration(String regData){
-        String msg="";
-        String[] regDataArr = regData.split(" ");
-        try {
-            if(isUserWithLogin(regDataArr[1])){
-                msg = "Пользователь с логином "+regDataArr[1]+ " уже зарегистрирован";
-            }else if(isUserWithNick(regDataArr[3])){
-                msg = "Пользователь с ником "+regDataArr[3]+ " уже зарегистрирован";
-            }else {
-                String sql = String.format("INSERT INTO main (login, password, nickname, controlword)\n" +
-                        "VALUES ('%s', '%s','%s','%s');", regDataArr[1], regDataArr[2], regDataArr[3], regDataArr[4]);
-                stmt.execute(sql);
-                msg = "/regok";
-            }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+    private static int getId(String nickname) throws SQLException {
+        int res = -1;
+        String sql = String.format("SELECT id FROM main WHERE nickname = '%s'",nickname);
+        ResultSet rs = stmt.executeQuery(sql);
+        if(rs.next()){
+            res = rs.getInt(1);
         }
-        return msg;
+        return res;
     }
+
+//    private static void updateBlackList(String blacklist, )
+//убрать метод в контроллер регистрации
+//    static synchronized String registration(String regData){
+//        String msg="";
+//        String[] regDataArr = regData.split(" ");
+//        try {
+//            if(isUserWithLogin(regDataArr[1])){
+//                msg = "Пользователь с логином "+regDataArr[1]+ " уже зарегистрирован";
+//            }else if(isUserWithNick(regDataArr[3])){
+//                msg = "Пользователь с ником "+regDataArr[3]+ " уже зарегистрирован";
+//            }else {
+//                writeRegDataToSQLite(regDataArr[1], regDataArr[2], regDataArr[3], regDataArr[4]);
+//                msg = "/regok";
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return msg;
+//    }
+
+    private static void writeRegDataToSQLite(String login, String password, String nickname, String controlword)
+            throws SQLException {
+        String sql = String.format("INSERT INTO main (login, password, nickname, controlword)\n" +
+                "VALUES ('%s', '%s','%s','%s');", login, password, nickname, controlword);
+        stmt.execute(sql);
+    }
+
     static synchronized String recoveryPass(String recoveryData){
         String resMsg = "/recovery ";
         String[] recoveryDataArr = recoveryData.split(" ");
@@ -163,8 +151,8 @@ class DataBaseService {
 
 
     static void clearBlackList(String holderBlackList){
-        String sql = String.format("UPDATE main SET blacklist = NULL WHERE nickname = '%s'",holderBlackList);
         try {
+            String sql = String.format("DELETE FROM blacklist WHERE id_nick = %s",getId(holderBlackList));
             stmt.execute(sql);
         } catch (SQLException e) {
             e.printStackTrace();
