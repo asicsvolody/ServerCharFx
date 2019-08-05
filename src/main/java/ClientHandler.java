b99f78e5b7fc736061b51be06a8f76926ce8c177
Vladimir Iakimov <yakimovvn@bk.ru>
2019-08-06 01:12:16 +0300

    Отредактировал класс ClientHandler, увеличил читаемость кода.

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -25,103 +25,11 @@ public class ClientHandler {
 
             new Thread(()-> {
                 try {
-                    while (true) {
-                        String str = in.readUTF();
-                        if (str.equals("/end")) {
-                            out.writeUTF("/serverclosed");
-                            break;
-                        }
-                        if(str.startsWith("/registration")){
-                            out.writeUTF(registration(str));
-                            server.writeToAdminLogInfo("Client try to registration with data: "+str);
-                        }
-                        if(str.startsWith("/recovery")){
-                            out.writeUTF(DataBaseService.recoveryPass(str));
-                            server.writeToAdminLogInfo("Client try to recovery password with data: "+str);
+                    loginAndRegCycle();
 
+                    mainWorkCycle();
 
-                        }
-                        if(str.startsWith("/auth")) {
-                            String[] tokens = str.split(" ");
-                            String newNick = DataBaseService.getNickByLoginAndPass(tokens[1], tokens[2]);
-                            if (newNick != null) {
-                                if(!server.isNickBusy(newNick)){
-                                    sendMsg("/authok " +newNick);
-                                    nick = newNick;
-                                    server.writeToAdminLogInfo(nick + "- log in is ok ");
-                                    server.subscribe(ClientHandler.this);
-                                    break;
-                                } else {
-                                    sendMsg("Пользователь в сети.");
-                                    server.writeToAdminLogWarn("Attempt to log in with client in net with login:"+tokens[1]);
-                                }
-                            }else {
-                                sendMsg("Неверный логин/пароль");
-                                server.writeToAdminLogWarn(String.format(
-                                        "Attempt to log in with wrong login:%s, password:%s",tokens[1],tokens[2]));
-                            }
-                        }
-                    }
-
-                    while (nick!=null){
-                        String str = in.readUTF();
-                        if(str.startsWith("/")) {
-                            if (str.equals("/end")) {
-                                out.writeUTF("/serverclosed");
-                                break;
-                            }
-                            if(str.startsWith("/w")){
-                                String[] tokens = str.split(" ",3);
-                                server.sendPersonalMsg(ClientHandler.this,tokens[1],tokens[2]);
-                            }
-                            if(str.startsWith("/blacklist")){
-                                String[] tokens = str.split(" ");
-                                if(tokens.length == 1){
-                                    sendMsg("/systemmsg command not found");
-                                }
-                                else if(nick.equals(tokens[1])){
-                                    sendMsg("/systemmsg Вы не можете добавить в черный список самого себя");
-                                }
-                                else if(DataBaseService.isUserWithNick(tokens[1])) {
-                                    if (DataBaseService.isInBlackList(nick, tokens[1])) {
-                                        sendMsg("/systemmsg пользователь уже в черном списке");
-                                    } else {
-                                        DataBaseService.addToBlackList(nick, tokens[1]);
-//                                            sendMsg("/systemmsg Вы добавили пользователя " + tokens[1] + " в черный список");
-                                        server.broadcastSystemMsg(nick+" добавил "+tokens[1]+ " в черный список");
-                                    }
-                                }else{
-                                    sendMsg("/systemmsg Пользователь с ником "+tokens[1]+" не зарегистрирован");
-                                }
-                            }
-                            if(str.startsWith("/delblacklist")){
-                                String[] tokens = str.split(" ");
-                                if(tokens.length ==1){
-                                    sendMsg("/systemmsg вы не ввели ник пользователя");
-
-                                }
-                                else if (DataBaseService.isInBlackList(nick, tokens[1])) {
-                                    DataBaseService.deleteFromBlackList(nick, tokens[1]);
-//                                        sendMsg("Вы удалили пользователя " + tokens[1] + " из черного списока");
-                                    server.broadcastSystemMsg(tokens[1]+ " теперь модет общаться с "+nick);
-
-                                } else {
-                                    sendMsg("/systemmsg Пользователя "+ tokens[1]+" нет в черном списке");
-
-                                }
-                            }
-                            if(str.startsWith("/clearblacklist")){
-                                DataBaseService.clearBlackList(nick);
-                                sendMsg("/systemmsg Черный список очищен.");
-                            }
-                        }else {
-                            server.broadcastMsg(ClientHandler.this,nick+" "+str);
-                            DataBaseService.writeMessageToSQLite(nick, str);
-
-                        }
-                }
             } catch (IOException e) {
-//                         e.printStackTrace();
                     server.writeToAdminLogError(server.stackTraceToString(e.getStackTrace()));
 
                 }finally {
@@ -153,6 +61,125 @@ public class ClientHandler {
         } catch (IOException e) {
             server.writeToAdminLogError(server.stackTraceToString(e.getStackTrace()));
         }
+    }
+
+    private void loginAndRegCycle() throws IOException {
+        while (true) {
+            String str = in.readUTF();
+            if (str.equals("/end")) {
+                out.writeUTF("/serverclosed");
+                break;
+            }
+            if(str.startsWith("/registration")){
+               tryToRegistration(str);
+            }
+            if(str.startsWith("/recovery")){
+                tryToRecovery(str);
+            }
+            if(str.startsWith("/auth")) {
+                String[] tokens = str.split(" ");
+                String newNick = DataBaseService.getNickByLoginAndPass(tokens[1], tokens[2]);
+                if (newNick != null) {
+                    if(!server.isNickBusy(newNick)){
+                        sendMsg("/authok " +newNick);
+                        nick = newNick;
+                        server.writeToAdminLogInfo(nick + "- log in is ok ");
+                        server.subscribe(ClientHandler.this);
+                        break;
+                    } else {
+                        sendMsg("Пользователь в сети.");
+                        server.writeToAdminLogWarn("Attempt to log in with client in net with login:"+tokens[1]);
+                    }
+                }else {
+                    sendMsg("Неверный логин/пароль");
+                    server.writeToAdminLogWarn(String.format(
+                            "Attempt to log in with wrong login:%s, password:%s",tokens[1],tokens[2]));
+                }
+            }
+        }
+    }
+
+    private void tryToRegistration(String str) throws IOException {
+        out.writeUTF(registration(str));
+        server.writeToAdminLogInfo("Client try to registration with data: "+str);
+    }
+
+    private void tryToRecovery(String str) throws IOException {
+        out.writeUTF(DataBaseService.recoveryPass(str));
+        server.writeToAdminLogInfo("Client try to recovery password with data: "+str);
+    }
+
+
+    private void mainWorkCycle() throws IOException {
+        while (nick!=null){
+            String str = in.readUTF();
+            if(str.startsWith("/")) {
+                if (str.equals("/end")) {
+                    out.writeUTF("/serverclosed");
+                    break;
+                }
+                if(str.startsWith("/w")){
+                   workWithPrivateMsg(str);
+                }
+                if(str.startsWith("/blacklist")){
+                    addToBlacklist(str);                }
+                if(str.startsWith("/delblacklist")){
+                    deleteFromBlackList(str);
+                }
+                if(str.startsWith("/clearblacklist")){
+                    clearBlackList();
+                }
+            }else {
+                server.broadcastMsg(ClientHandler.this,nick+" "+str);
+                DataBaseService.writeMessageToSQLite(nick, str);
+
+            }
+        }
+    }
+
+    private void workWithPrivateMsg(String str){
+        String[] tokens = str.split(" ",3);
+        server.sendPersonalMsg(ClientHandler.this,tokens[1],tokens[2]);
+    }
+
+    private void addToBlacklist(String str){
+        String[] tokens = str.split(" ");
+        if(tokens.length == 1){
+            sendMsg("/systemmsg command not found");
+        }
+        else if(nick.equals(tokens[1])){
+            sendMsg("/systemmsg Вы не можете добавить в черный список самого себя");
+        }
+        else if(DataBaseService.isUserWithNick(tokens[1])) {
+            if (DataBaseService.isInBlackList(nick, tokens[1])) {
+                sendMsg("/systemmsg пользователь уже в черном списке");
+            } else {
+                DataBaseService.addToBlackList(nick, tokens[1]);
+                server.broadcastSystemMsg(nick+" добавил "+tokens[1]+ " в черный список");
+            }
+        }else{
+            sendMsg("/systemmsg Пользователь с ником "+tokens[1]+" не зарегистрирован");
+        }
+    }
+
+    private void deleteFromBlackList(String str){
+        String[] tokens = str.split(" ");
+        if(tokens.length ==1){
+            sendMsg("/systemmsg вы не ввели ник пользователя");
+        }
+        else if (DataBaseService.isInBlackList(nick, tokens[1])) {
+            DataBaseService.deleteFromBlackList(nick, tokens[1]);
+            server.broadcastSystemMsg(tokens[1]+ " теперь может общаться с "+nick);
+
+        } else {
+            sendMsg("/systemmsg Пользователя "+ tokens[1]+" нет в черном списке");
+
+        }
+    }
+
+    private void clearBlackList(){
+        DataBaseService.clearBlackList(nick);
+        sendMsg("/systemmsg Черный список очищен.");
     }
 
     private String registration(String regData){
